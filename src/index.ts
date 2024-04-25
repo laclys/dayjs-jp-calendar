80b52e65c288094bad883d75872d9092e83844ae
Lac <laclys@126.com>
2024-04-25 22:18:35 +0900

    fix:some bugs

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,32 @@
-import { PluginFunc, Dayjs } from 'dayjs'
+import { PluginFunc } from 'dayjs'
 
-/* Eg. 
-２０２４年
-rrrr　→　令和６年 
-rrr　→　令和
-rr →　R6 
-r →　R 
- */
-type FormatStr = 'RRRR' | 'RR' | 'rrrr' | 'rr' | string
+type FormatStr = 'rrrr' | 'rr' | 'rrrr' | 'rr' | string
 
-const format = (str: string, dayjs: Dayjs) => {
-  str.replace(/\[([^\]]+)]|r+|/g, (match) => {
-    const date = dayjs.toDate()
-    const ret = Intl.DateTimeFormat('ja-JP-u-ca-japanese', {
-      year: '2-digit',
-      era: 'long'
-    }).format(date)
-    const digitRet = Intl.DateTimeFormat('ja-JP-u-ca-japanese').format(date)
-    switch (match) {
-      case 'rrrr':
-        return ret
-      case 'rrr':
-        return ret.slice(0, 2)
-      case 'rr':
-        return digitRet.slice(0, 2)
-      case 'r':
-        return digitRet.slice(0, 1)
-      default:
-        return match
-    }
-  })
-}
+export const jpFormat: PluginFunc = function (_o, c) {
+  const proto = c.prototype
+  const oldFormat = proto.format
 
-export const jpCalendar: PluginFunc = (_option, dayjsClass, _dayjsFactory) => {
-  const oldFormat = dayjsClass.prototype.format
-  const proto = dayjsClass.prototype
-  proto.format = function (template) {
-    const ret = template ? format(template, this) : template
-    return oldFormat.bind(this)(ret)
+  proto.format = function (formatStr: FormatStr) {
+    const result = formatStr.replace(/\[([^\]]+)]|r+|/g, (match) => {
+      const date = this.toDate()
+      const ret = Intl.DateTimeFormat('ja-JP-u-ca-japanese', {
+        year: '2-digit',
+        era: 'long'
+      }).format(date)
+      const digitRet = Intl.DateTimeFormat('ja-JP-u-ca-japanese').format(date)
+      switch (match) {
+        case 'rrrr':
+          return ret
+        case 'rrr':
+          return ret.slice(0, 2)
+        case 'rr':
+          return digitRet.slice(0, 2)
+        case 'r':
+          return digitRet.slice(0, 1)
+        default:
+          return match
+      }
+    })
+    return oldFormat.bind(this)(result)
   }
 }
-
-export default jpCalendar
