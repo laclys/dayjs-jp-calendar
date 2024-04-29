8d5dbeaf1c63b1448875d19046cd3ed03eb52122
Lac <laclys@126.com>
2024-04-29 16:09:46 +0900

    fix:bugs

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,48 @@
-import { PluginFunc } from 'dayjs'
+import { PluginFunc, Dayjs } from 'dayjs'
 
 type FormatStr = 'rrrr' | 'rr' | 'rrrr' | 'rr' | string
 
-export const jpFormat: PluginFunc = function (_o, c) {
+/* 
+ isShort: true  -> 令和
+ isShort: false  -> 令和X
+*/
+const kanji = (date: Dayjs, isShort: boolean) => {
+  const targetDay = date.toDate()
+  const ret = Intl.DateTimeFormat('ja-JP-u-ca-japanese', {
+    year: '2-digit',
+    era: 'long'
+  }).format(targetDay)
+  return !isShort ? ret.slice(0, 3) : ret.slice(0, 2)
+}
+
+/* 
+ isShort: true  -> R
+ isShort: false  -> RX
+*/
+const code = (date: Dayjs, isShort: boolean) => {
+  const targetDay = date.toDate()
+  const ret = Intl.DateTimeFormat('ja-JP-u-ca-japanese').format(targetDay)
+  return !isShort ? ret.slice(0, 2) : ret.slice(0, 1)
+}
+
+export const jpCalendar: PluginFunc = function (_o, c) {
   const proto = c.prototype
   const oldFormat = proto.format
 
   proto.format = function (formatStr: FormatStr) {
     const result = formatStr.replace(/\[([^\]]+)]|r+|/g, (match) => {
+      console.log('type', typeof this)
       const date = this.toDate()
-      const ret = Intl.DateTimeFormat('ja-JP-u-ca-japanese', {
-        year: '2-digit',
-        era: 'long'
-      }).format(date)
       const digitRet = Intl.DateTimeFormat('ja-JP-u-ca-japanese').format(date)
       switch (match) {
         case 'rrrr':
-          return ret
+          return kanji(this, false)
         case 'rrr':
-          return ret.slice(0, 2)
+          return kanji(this, true)
         case 'rr':
-          return digitRet.slice(0, 2)
+          return code(this, false)
         case 'r':
-          return digitRet.slice(0, 1)
+          return code(this, true)
         default:
           return match
       }
@@ -30,3 +50,5 @@ export const jpFormat: PluginFunc = function (_o, c) {
     return oldFormat.bind(this)(result)
   }
 }
+
+export default jpCalendar
