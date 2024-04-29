3d9baf48b331014339d5f9d0a33d6a76c2651edc
Lac <laclys@126.com>
2024-04-29 22:43:13 +0900

    fix: some bugs && rebuild

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,16 @@ type FormatStr = 'rrrr' | 'rr' | 'rrrr' | 'rr' | string
  isShort: false  -> 令和X
 */
 const kanji = (date: Dayjs, isShort: boolean) => {
-  const targetDay = date.toDate()
-  const ret = Intl.DateTimeFormat('ja-JP-u-ca-japanese', {
-    year: '2-digit',
-    era: 'long'
-  }).format(targetDay)
-  return !isShort ? ret.slice(0, 3) : ret.slice(0, 2)
+  try {
+    const targetDay = date.toDate()
+    const ret = Intl.DateTimeFormat('ja-JP-u-ca-japanese', {
+      year: '2-digit',
+      era: 'long'
+    }).format(targetDay)
+    return !isShort ? ret.slice(0, 3) : ret.slice(0, 2)
+  } catch {
+    return 'unknown'
+  }
 }
 
 /* 
@@ -20,9 +24,13 @@ const kanji = (date: Dayjs, isShort: boolean) => {
  isShort: false  -> RX
 */
 const code = (date: Dayjs, isShort: boolean) => {
-  const targetDay = date.toDate()
-  const ret = Intl.DateTimeFormat('ja-JP-u-ca-japanese').format(targetDay)
-  return !isShort ? ret.slice(0, 2) : ret.slice(0, 1)
+  try {
+    const targetDay = date.toDate()
+    const ret = Intl.DateTimeFormat('ja-JP-u-ca-japanese').format(targetDay)
+    return !isShort ? ret.slice(0, 2) : ret.slice(0, 1)
+  } catch {
+    return 'unknown'
+  }
 }
 
 export const jpCalendar: PluginFunc = function (_o, c) {
