8c8e4d0cd15d067e586d5f5d3a36782017e923ad
Lac <laclys@126.com>
2024-05-11 10:50:32 +0900

    feat: Change 'r' to 'R' to fix 'Meiji M' and 'Heisei H' display && Change Format String from 'r' to 'R'

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { PluginFunc, Dayjs } from 'dayjs'
 
-type FormatStr = 'rrrr' | 'rr' | 'rrrr' | 'rr' | string
+type FormatStr = 'RRRR' | 'RRR' | 'RR' | 'R' | string
 
 /* 
  isShort: true  -> 令和
@@ -13,7 +13,7 @@ const kanji = (date: Dayjs, isShort: boolean) => {
       year: '2-digit',
       era: 'long'
     }).format(targetDay)
-    return !isShort ? ret.slice(0, 3) : ret.slice(0, 2)
+    return !isShort ? ret.slice(0, -1) : ret.slice(0, 2)
   } catch {
     return 'unknown'
   }
@@ -26,8 +26,27 @@ const kanji = (date: Dayjs, isShort: boolean) => {
 const code = (date: Dayjs, isShort: boolean) => {
   try {
     const targetDay = date.toDate()
-    const ret = Intl.DateTimeFormat('ja-JP-u-ca-japanese').format(targetDay)
-    return !isShort ? ret.slice(0, 2) : ret.slice(0, 1)
+    const ret = Intl.DateTimeFormat('ja-JP-u-ca-japanese', {
+      era: 'short'
+    })
+      .format(targetDay)
+      .replace(/明治|大正|昭和|平成|令和/g, (match) => {
+        switch (match) {
+          case '明治':
+            return 'Ｍ'
+          case '大正':
+            return 'T'
+          case '昭和':
+            return 'S'
+          case '平成':
+            return 'Ｈ'
+          case '令和':
+            return 'R'
+          default:
+            return match
+        }
+      })
+    return !isShort ? ret.split('/')[0] : ret.slice(0, 1)
   } catch {
     return 'unknown'
   }
@@ -38,21 +57,23 @@ export const jpCalendar: PluginFunc = function (_o, c) {
   const oldFormat = proto.format
 
   proto.format = function (formatStr: FormatStr) {
-    const result = formatStr.replace(/\[([^\]]+)]|r+|/g, (match) => {
+    const result = formatStr.replace(/(\[[^\]]+])|RRRR|RRR|RR|R/g, (match) => {
       switch (match) {
-        case 'rrrr':
+        case 'RRRR':
           return kanji(this, false)
-        case 'rrr':
+        case 'RRR':
           return kanji(this, true)
-        case 'rr':
+        case 'RR':
           return code(this, false)
-        case 'r':
+        case 'R':
           return code(this, true)
         default:
+          console.log('match', match)
           return match
       }
     })
-    return oldFormat.bind(this)(result)
+    /* This code handles the predefined "H" and "M" fields within Day.js */
+    return oldFormat.bind(this)(result).replace(/Ｈ/g, 'H').replace(/Ｍ/g, 'M')
   }
 }
 
