2d34a03bc38aa4a93f4139e0df9105733acd822b
Lac <laclys@126.com>
2024-05-17 11:01:24 +0900

    chore: Added test cases to ensure compatibility

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -29,6 +29,16 @@ test(`令和のCase: format("YYYY(RR)/MM/DD")`, () => {
   expect(dayjs(date).format('YYYY(RR)/MM/DD')).toBe('2023(R5)/11/13')
 })
 
+test(`令和のCase: format("YYYY(rr)/MM/DD")`, () => {
+  const date = '2023-11-13'
+  expect(dayjs(date).format('YYYY(rr)/MM/DD')).toBe('2023(㋿5)/11/13')
+})
+
+test(`令和のCase: format("r")`, () => {
+  const date = '2023-11-13'
+  expect(dayjs(date).format('r')).toBe('㋿')
+})
+
 test(`平成のCase: format("RRRR")`, () => {
   const date = '1994-11-13'
   expect(dayjs(date).format('RRRR')).toBe('平成6')
@@ -39,31 +49,67 @@ test(`平成のCase: format("YYYY(RR)")`, () => {
   expect(dayjs(date).format('YYYY(RR)')).toBe('1994(H6)')
 })
 
+test(`平成のCase: format("YYYY(rr)")`, () => {
+  const date = '1994-11-13'
+  expect(dayjs(date).format('YYYY(rr)')).toBe('1994(㍻6)')
+})
+
 test(`昭和のCase: format("YYYY(RR)")`, () => {
   const date = '1973-11-13'
   expect(dayjs(date).format('YYYY(RR)')).toBe('1973(S48)')
 })
 
-test(`明治のCase`, () => {
-  const date = '1889-11-13'
-  console.log('123', dayjs(date).format('YYYY(RR)'))
-  expect(dayjs(date).format('YYYY(RR)')).toBe('1889(M22)')
+test(`昭和のCase: format("YYYY(RR)rr")`, () => {
+  const date = '1973-11-13'
+  expect(dayjs(date).format('YYYY(RR)rr')).toBe('1973(S48)㍼48')
 })
 
 test(`大正のCase`, () => {
   const date = '1920-11-13'
-  console.log('123', dayjs(date).format('YYYY(RRRR)'))
   expect(dayjs(date).format('YYYY(RR)')).toBe('1920(T9)')
 })
 
-test(`Unknow`, () => {
+test(`大正のCase`, () => {
+  const date = '1920-11-13'
+  expect(dayjs(date).format('YYYY(rr)')).toBe('1920(㍽9)')
+})
+
+test(`明治のCase`, () => {
+  const date = '1889-11-13'
+  expect(dayjs(date).format('YYYY(RR)')).toBe('1889(M22)')
+})
+
+test(`明治のCase`, () => {
+  const date = '1889-11-13'
+  expect(dayjs(date).format('YYYY(rr)')).toBe('1889(㍾22)')
+})
+
+test(`The year is below the minimum year(明治元年) - format('YYYY(RR)')`, () => {
   const date = '199-11-13'
-  console.log('123', dayjs(date).format('YYYY(RR)'))
-  expect(dayjs(date).format('YYYY(RR)')).toBe('0199(大化-445)')
+  expect(dayjs(date).format('YYYY(RR)')).toBe('0199(-)')
+})
+
+test(`The year is below the minimum year(明治元年) - format('YYYY(R)'))`, () => {
+  const date = '199-11-13'
+  expect(dayjs(date).format('YYYY(R)')).toBe('0199(-)')
+})
+
+test(`The year is below the minimum year(明治元年) - format('YYYY(RRRR)')`, () => {
+  const date = '1773-11-13'
+  expect(dayjs(date).format('YYYY(RRRR)')).toBe('1773(-)')
+})
+
+test(`The year is below the minimum year(明治元年)`, () => {
+  const date = '1773-11-13'
+  expect(dayjs(date).format('YYYY(RR)')).toBe('1773(-)')
+})
+
+test(`The year is below the minimum year(明治元年) - 'YYYY(rr)`, () => {
+  const date = '1773-11-13'
+  expect(dayjs(date).format('YYYY(rr)')).toBe('1773(-)')
 })
 
 test(`Unknow`, () => {
   const date = '1993-11-13'
-  console.log('123', dayjs(date).format('YYYY(RRRRR)'))
   expect(dayjs(date).format('YYYY(RRRRR)')).toBe('1993(平成5H)')
 })
